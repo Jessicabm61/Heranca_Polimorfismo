d02e572bccc33aa80b4c2014534d761ce595bfc1
Jéssica <jessicabeatrizmonteiro1994@gmail.com>
2022-01-01 10:14:23 -0300

    my first commit

---
+++ Exercício15/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,22 @@
+﻿namespace Entities.Exercício15
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AddictionalCharge { get; set; }
+        public OutsourcedEmployee ()
+        {
+        }
+
+        public OutsourcedEmployee (string name, int hour, double valuePerHour, double addictionalCharge):
+        base (name, hour, valuePerHour)
+        {
+            AddictionalCharge = addictionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AddictionalCharge;
+        }
+
+    }
+}
